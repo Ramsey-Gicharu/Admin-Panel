a2c4a50b45d47f18786ac808506357ae0c08e913
Ramsey Gicharu <89084952+Ramsey-Gicharu@users.noreply.github.com>
2022-09-02 01:38:02 +0300

    updates

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,55 @@
 import './widget.scss'
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 
-const widget = () => {
+const widget = ({type}) => {
+  let data;
+
+  switch(type){
+    case "user":
+      data ={
+        title: "USERS",
+        isMoney:false,
+        link: "see all users",
+        icon: <PersonOutlinedIcon className='icon'/>
+      };
+      break;
+         case "order":
+      data ={
+        title: "USERS",
+        isMoney:false,
+        link: "see all users",
+        icon: <PersonOutlinedIcon className='icon'/>
+      };
+      break;
+
+         case "user":
+      data ={
+        title: "USERS",
+        isMoney:false,
+        link: "see all users",
+        icon: <PersonOutlinedIcon className='icon'/>
+      };
+      break;
+    
+      
+
+
+  }
   return (
     <div className='widget'>
-        <div className="left">left</div>
-        <div className="right">right</div>
+        <div className="left">
+          <span className='title'>USERS</span>
+          <span className='counter'>2132</span>
+          <span className='link'>See all users</span>
+        </div>
+        <div className="right">
+          <div className="percentage positive">
+            <ArrowDropUpIcon />
+            20%
+          </div>
+          <PersonOutlinedIcon  className='icon'/>
+        </div>
        
         </div>
   )
